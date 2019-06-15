84871864e434a5fd12cbd4b350bd53f0ec4249d9
Zaid Baig <ZaidBaig@cmail.carleton.ca>
2019-06-15 18:36:42 -0400

    Framework Complete (With Comments).

--- A06_ZaidBaig_PyramidBuilder/A06_ZaidBaig_PyramidBuilder/Main.cpp
+++ A06_ZaidBaig_PyramidBuilder/A06_ZaidBaig_PyramidBuilder/Main.cpp
@@ -1,24 +1,36 @@
+// Zaid Baig 
+// BIT 2400
+// Assignment 6
+
+
+// Libraries
 #include <iostream>
 using namespace std;
 
 
 void main()
 {
+	// Variables & Declarations
 	int rows, count = 0, count1 = 0, k = 0;
 
+	// Welcoming & Prompting the User
 	cout << "Welcome to Pyramid Maker!\n\n";
 	cout << "Enter number of rows: ";
 	cin >> rows;
 
 	cout << "\n\n";
+
+	// Printing rows
 	for (int i = 1; i <= rows; ++i)
 	{
+		// Printing Space
 		for (int space = 1; space <= rows - i; ++space)
 		{
 			cout << "  ";
 			++count;
 		}
 
+		// Subtracting Row & Space Size after it prints
 		while (k != 2 * i - 1)
 		{
 			if (count <= rows - 1)
@@ -35,6 +47,7 @@ void main()
 		}
 		count1 = count = k = 0;
 
+		// End of Program
 		cout << endl;
 	}
 

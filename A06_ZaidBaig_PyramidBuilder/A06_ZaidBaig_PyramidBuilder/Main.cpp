27a3edea4c8b8f1b7d8bce1ad491539a9289df63
Zaid Baig <ZaidBaig@cmail.carleton.ca>
2019-06-15 18:42:37 -0400

    Final Project

--- A06_ZaidBaig_PyramidBuilder/A06_ZaidBaig_PyramidBuilder/Main.cpp
+++ A06_ZaidBaig_PyramidBuilder/A06_ZaidBaig_PyramidBuilder/Main.cpp
@@ -47,24 +47,27 @@ void main()
 		}
 		count1 = count = k = 0;
 
-		// End of Program
 		cout << endl;
 	}
 
 	cout << "\n\n";
 	rows = 0;
 
+	// Welcoming & Prompting the User
 	cout << "Welcome to the Opposite Pyramid Maker!\n\n";
 	cout << "Enter number of rows: ";
 	cin >> rows;
 
 	cout << "\n\n";
 
+	// Printing rows
 	for (int i = rows; i >= 1; --i)
 	{
+		// Printing Space
 		for (int space = 0; space < rows - i; ++space)
 			cout << "  ";
 
+		// Subtracting Row & Space Size after it prints
 		for (int j = i; j <= 2 * i - 1; ++j)
 			cout << "* ";
 
@@ -73,5 +76,5 @@ void main()
 
 		cout << endl;
 	}
-
+	// End of Program
 }
